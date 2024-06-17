25e60d1c45393d88b8f4aa4eb03f775c5b5ea05d
nehcgnak <alwaysbecalm@gmail.com>
2024-06-17 01:35:46 +0000

    Homework4 Solution

--- 04-deployment/homework/score.py
+++ 04-deployment/homework/score.py
@@ -1,39 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-get_ipython().system('pip freeze | grep scikit-learn')
-
-
-# In[2]:
-
-
-get_ipython().system('python -V')
-
-
-# In[3]:
-
 
 import pickle
 import pandas as pd
 import numpy as np
+import sys
 
 
-# In[4]:
-
-
-with open('model.bin', 'rb') as f_in:
-    dv, model = pickle.load(f_in)
-
-
-# In[5]:
-
-
-categorical = ['PULocationID', 'DOLocationID']
-
-def read_data(filename):
+def read_data(filename, categorical):
     df = pd.read_parquet(filename)
     
     df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
@@ -46,69 +21,45 @@ def read_data(filename):
     return df
 
 
-# In[6]:
+def predict(df, categorical):
+    with open('model.bin', 'rb') as f_in:
+        dv, model = pickle.load(f_in)
+
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = model.predict(X_val)
+
+    return y_pred
 
 
-df = read_data('https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-03.parquet')
-df
+def run():
+    taxi_type = sys.argv[1] 
+    year = int(sys.argv[2]) 
+    month = int(sys.argv[3]) 
+
+    input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/{taxi_type}_tripdata_{year:04d}-{month:02d}.parquet'
+    output_file = f'{taxi_type}-{year:04d}-{month:02d}.parquet'
+
+    categorical = ['PULocationID', 'DOLocationID']
+
+    df = read_data(input_file, categorical)
+    df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+    y_pred = predict(df, categorical)
+
+    df_result = pd.DataFrame()
+    df_result['predictions'] = y_pred.tolist()
+    df_result['ride_id'] = df['ride_id'].tolist()
+
+    df_result.to_parquet(output_file, 
+                         engine='pyarrow', 
+                         compression=None,
+                         index=False
+                         )
+
+    print(np.mean(y_pred))
 
 
-# In[7]:
-
-
-dicts = df[categorical].to_dict(orient='records')
-X_val = dv.transform(dicts)
-y_pred = model.predict(X_val)
-
-
-# In[8]:
-
-
-np.std(y_pred)
-
-
-# In[9]:
-
-
-y_pred
-
-
-# In[11]:
-
-
-year = 2023
-month = 3
-df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
-
-
-# In[12]:
-
-
-df_result = pd.DataFrame()
-df_result['predictions'] = y_pred.tolist()
-df_result['ride_id'] = df['ride_id'].tolist()
-df_result
-
-
-# In[13]:
-
-
-get_ipython().system('pip install pyarrow')
-
-
-# In[15]:
-
-
-df_result.to_parquet(
-    'df_result.parquet',
-    engine='pyarrow',
-    compression=None,
-    index=False
-)
-
-
-# In[ ]:
-
-
+if __name__ == '__main__':
+    run()
 
 
